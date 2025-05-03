8c5d6381a295c0f6b1dd3be1f0a80375f4a4389c
Oussama Mimouni <omimouni33@gmail.com>
2025-05-04 00:00:49 +0100

    update

---
+++ inception/srcs/requirements/wordpress/wp-config.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Install wp-cli
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
+
+# Prepare WordPress directory
+mkdir -p /var/www/wordpress
+cd /var/www/wordpress
+chmod -R 755 /var/www/wordpress/
+chown -R www-data:www-data /var/www/wordpress
+
+# Download and configure WordPress
+wp core download --allow-root
+
+wp core config \
+    --dbhost=mariadb:3306 \
+    --dbname="$MYSQL_DATABASE" \
+    --dbuser="$MYSQL_USER" \
+    --dbpass="$MYSQL_PASSWORD" \
+    --allow-root
+
+wp core install \
+    --url="$DOMAIN_NAME" \
+    --title="$WP_TITLE" \
+    --admin_user="$WP_ADMIN_USERNAME" \
+    --admin_password="$WP_ADMIN_PASSWORD" \
+    --admin_email="$WP_ADMIN_EMAIL" \
+    --allow-root
+
+wp user create \
+    "$WP_USER_USERNAME" "$WP_USER_EMAIL" \
+    --user_pass="$WP_USER_PASSWORD" \
+    --role="$WP_USER_ROLE" \
+    --allow-root
+
+# PHP-FPM config
+sed -i 's|^listen = .*|listen = 9000|' /etc/php/7.4/fpm/pool.d/www.conf
+mkdir -p /run/php
+
+# Run PHP-FPM in foreground
+/usr/sbin/php-fpm7.4 -F
