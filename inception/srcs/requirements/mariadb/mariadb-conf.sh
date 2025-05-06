d8db740c40b2b1b8e88ca7baef7b2d815afd3991
Oussama Mimouni <omimouni33@gmail.com>
2025-05-06 16:06:24 +0100

    optimized mariadb

--- inception/srcs/requirements/mariadb/mariadb-conf.sh
+++ inception/srcs/requirements/mariadb/mariadb-conf.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 
-set -ex
+set -e
 
 service mariadb start
 sleep 5
 
 echo "MYSQL_DATABASE: \`${MYSQL_DATABASE}\`"
 echo "MYSQL_USER: \`${MYSQL_USER}\`"
-echo "MYSQL_PASSWORD: \`${MYSQL_PASSWORD}\`"
 
-
-mariadb -uroot -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
-
-mariadb -uroot -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-
-mariadb -uroot -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%';"
-
-mariadb -uroot -e "FLUSH PRIVILEGES;"
+mariadb -uroot <<EOF
+CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
+CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%';
+FLUSH PRIVILEGES;
+EOF
 
 mysqladmin -uroot shutdown
 
-exec mysqld_safe --bind-address=0.0.0.0
+mysqld_safe --mysqld="--bind-address=0.0.0.0" # -> need to connect remotely
