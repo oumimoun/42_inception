011553eccba905eb9e71d3cbad577a1447f4b9b7
Oussama Mimouni <omimouni33@gmail.com>
2025-05-08 21:31:30 +0100

    updated

--- inception/srcs/requirements/wordpress/conf/wp-config.sh
+++ inception/srcs/requirements/wordpress/conf/wp-config.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 
 mkdir -p /var/www/wordpress
 cd /var/www/wordpress
-chmod -R 755 /var/www/wordpress/
 chown -R www-data:www-data /var/www/wordpress
+chmod -R 755 /var/www/wordpress/
 
 wp core download --allow-root
 
-wp core config \
-    --dbhost=mariadb:3306 \
-    --dbname="$MYSQL_DATABASE" \
-    --dbuser="$MYSQL_USER" \
-    --dbpass="$MYSQL_PASSWORD" \
-    --allow-root
+wp config create \
+  --dbname="$MYSQL_DATABASE" \
+  --dbuser="$MYSQL_USER" \
+  --dbpass="$MYSQL_PASSWORD" \
+  --dbhost=mariadb:3306 \
+  --skip-check \
+  --allow-root
 
 wp core install \
     --url="$DOMAIN_NAME" \
@@ -35,4 +37,4 @@ wp user create \
 sed -i '36 s@/run/php/php7.4-fpm.sock@9000@' /etc/php/7.4/fpm/pool.d/www.conf
 mkdir -p /run/php
 
-/usr/sbin/php-fpm7.4 -F
+exec /usr/sbin/php-fpm7.4 -F
